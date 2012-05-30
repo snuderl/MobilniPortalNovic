be1f813ca1dfb2d91dd6ca63b430f2094bcee921
Snuderl <Snuderl@gmail.com>
2012-05-30 19:21:49 +0200

    Rss fixed

--- MobilniPortalNovic/Controllers/FeedController.cs
+++ MobilniPortalNovic/Controllers/FeedController.cs
@@ -18,11 +18,16 @@ namespace MobilniPortalNovic.Controllers
 
         public ActionResult Index()
         {
-            var articles = context.NewsFiles.OrderBy(pub => pub.PubDate).Take(15).ToList().Select(p => new SyndicationItem(p.Title, p.ShortContent, new Uri(Url.Action("Details", "NewsFiles", new { id = p.FeedId }, "http")).SetPort(80)));
+            var articles = context.NewsFiles.OrderBy(pub => pub.PubDate).Take(15).ToList().Select(p => new SyndicationItem(p.Title, p.ShortContent, uriMaker(p.FeedId), p.FeedId.ToString(), p.PubDate));
 
             var feed = new SyndicationFeed("Novice", "Your source to knowledge", new Uri(Url.Action("Index", "Home", new { }, "http")).SetPort(80), articles);
 
             return new FeedResult(new Rss20FeedFormatter(feed));
+        }
+
+        private Uri uriMaker(int id)
+        {
+            return new Uri(Url.Action("Details", "NewsFiles", new { id = id }, "http")).SetPort(80);
         }
 
         [HttpPost]
