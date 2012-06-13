bf36892d6a0ab67294f506a07f0af6dae777e379
Snuderl <Snuderl@gmail.com>
2012-06-13 12:34:56 +0200

    CategoryPersonalizer tests

--- MobilniPortalNovicLib/Personalize/CategoryPersonalizer.cs
+++ MobilniPortalNovicLib/Personalize/CategoryPersonalizer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MobilniPortalNovicLib.Helpers;
 using MobilniPortalNovicLib.Models;
@@ -28,19 +29,65 @@ namespace MobilniPortalNovicLib.Personalize
         public IQueryable<NewsFile> GetNews(User u)
         {
             var clicks = Context.Clicks.Include("NewsFile").Where(x => x.UserId == u.UserId).ToList();
-            var categories = new CategoryWraper(Context.Categories.ToList());
-            var count = CategoryHelpers.NumberOfCliksPerCategory(clicks.Select(x => x.NewsFile));
+            var goodCategories = GetDesiredCategories(clicks, CategoryTreshold);
 
+            return Context.NewsFiles.Where(x => goodCategories.Contains(x.CategoryId)).OrderByDescending(x => x.PubDate);
+        }
+
+        /// <summary>
+        /// Take IQueryable of clicks and choose good categories based on number of clicks per category and treeshold
+        /// </summary>
+        /// <param name="clicks">Clicks</param>
+        /// <param name="treshold">How much percent of clicks do you want to match</param>
+        /// <returns></returns>
+        public static HashSet<int> GetDesiredCategories(IEnumerable<ClickCounter> clicks, float treshold)
+        {
+            var count = CategoryHelpers.NumberOfCliksPerCategory(clicks.Select(x => x.NewsFile));
+            var totalClicks = clicks.Count();
             var goodCategories = new HashSet<int>();
             float total = 0;
             var enumerator = count.OrderByDescending(x => x.Value).GetEnumerator();
-            while(total<CategoryTreshold){
-                total += enumerator.Current.Value;
-                goodCategories.Add(enumerator.Current.Key);
+            while (total < treshold / 100)
+            {
                 enumerator.MoveNext();
+                total += ((float)enumerator.Current.Value) / totalClicks;
+                goodCategories.Add(enumerator.Current.Key);
+            }
+            return goodCategories;
+        }
+
+        public static IQueryable<ClickCounter> FilterClicksByDate(IQueryable<ClickCounter> clicks, DateTime target)
+        {
+            var timeOffsetHours = 1;
+            var upper = target.TimeOfDay.Add(new TimeSpan(timeOffsetHours, 0, 1));
+            var lower = target.TimeOfDay.Add(new TimeSpan(timeOffsetHours,0,1));
+
+
+            var clicksByHour = clicks.Where(x => x.ClickDate.TimeOfDay < upper && x.ClickDate.TimeOfDay > lower);
+
+            var workDays = new List<DayOfWeek> { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+            var weekendDays = new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+            var targetDays = new List<DayOfWeek>{target.DayOfWeek};
+            if (workDays.Contains(target.DayOfWeek))
+            {
+                int position = workDays.IndexOf(target.DayOfWeek);
+                if (position > 0)
+                {
+                    targetDays.Add(workDays[position - 1]);
+                }
+                if (position - 1 < workDays.Count - 1)
+                {
+                    targetDays.Add(workDays[position + 1]);
+                }
+            }
+            else
+            {
+                targetDays = weekendDays;
             }
 
-            return Context.NewsFiles.Where(x => goodCategories.Contains(x.CategoryId)).OrderByDescending(x => x.PubDate);
+            var clicksByDay = clicksByHour.ToList().Where(x => targetDays.Contains(x.ClickDate.DayOfWeek));
+            return clicksByDay.AsQueryable();
         }
     }
 }
---
+++ Tests/CategoryPersonalizerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobilniPortalNovicLib.Models;
+using MobilniPortalNovicLib.Personalize;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class CategoryPersonalizerTests
+    {
+        private Category cat1 =
+            new Category { CategoryId = 1, Name = "Šport", ParentCategoryId = new Nullable<int>() };
+
+        private Category cat2 =
+            new Category { CategoryId = 2, Name = "Novice", ParentCategoryId = new Nullable<int>() };
+
+        private Category cat3 =
+            new Category { CategoryId = 3, Name = "Novice", ParentCategoryId = 1 };
+
+        private Category cat4 = new Category { CategoryId = 4, ParentCategoryId = new Nullable<int>() };
+        private Category cat5 = new Category { CategoryId = 5, ParentCategoryId = 3 };
+        private Category cat6 = new Category { CategoryId = 6, ParentCategoryId = 3 };
+        NewsFile n1 = new NewsFile { NewsId = 1, CategoryId = 3 };
+        NewsFile n2 = new NewsFile { NewsId = 2, CategoryId = 2 };
+        NewsFile n3 = new NewsFile { NewsId = 3, CategoryId = 1 };
+        NewsFile n4 = new NewsFile { NewsId = 4, CategoryId = 2 };
+        NewsFile n5 = new NewsFile { NewsId = 5, CategoryId = 1 };
+        NewsFile n6 = new NewsFile { NewsId = 6, CategoryId = 2 };
+        private List<Category> categoriesSmall;
+        private List<ClickCounter> clicks;
+
+        [TestMethod]
+        public void MustAlwaysHaveAtleastCategoryWithMaxClicks()
+        {
+            var good = CategoryPersonalizer.GetDesiredCategories(clicks, 0.00001f);
+            Assert.AreEqual(good.Count, 1);
+            Assert.IsTrue(good.Contains(2));
+        }
+
+        [TestMethod]
+        public void ContainsMoreThanOneCategory()
+        {
+            var good = CategoryPersonalizer.GetDesiredCategories(clicks, 60f);
+            Assert.AreEqual(good.Count, 2);
+            Assert.IsTrue(good.Contains(2));
+            Assert.IsTrue(good.Contains(1));
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            categoriesSmall = new List<Category> { cat1, cat2, cat3 };
+            clicks = new List<ClickCounter>{
+                new ClickCounter{ NewsId=1, NewsFile=n1, ClickDate = DateTime.Now},
+                new ClickCounter{ NewsId=2, NewsFile=n2, ClickDate = DateTime.Now.AddHours(2)},
+                new ClickCounter{ NewsId=3, NewsFile=n3, ClickDate = DateTime.Now.AddHours(-3)},
+                new ClickCounter{ NewsId=4, NewsFile=n4, ClickDate = DateTime.Now.AddDays(1)},
+                new ClickCounter{ NewsId=5, NewsFile=n5, ClickDate = DateTime.Now.AddDays(2)},
+                new ClickCounter{ NewsId=6, NewsFile=n6, ClickDate = DateTime.Now.AddDays(3).AddHours(2)}
+            };
+
+        }
+
+        [TestMethod]
+        public void FilterClicksByDateTest()
+        {
+            var result = CategoryPersonalizer.FilterClicksByDate(clicks.AsQueryable(), DateTime.Now);
+            Assert.AreEqual(result.Count(), 3);
+        }
+    }
+}
